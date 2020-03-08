8b116c7376415bcd493085ffd7fe329051191f9f
harshil <sharshil43@yahoo.com>
2020-03-08 23:21:53 +0530

    init setup

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    socket.on('join', function (data) {
+        console.log("join called" + data.user)
+        socket.join(data.user);
+        //socket.userId = data.user
+    });
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
